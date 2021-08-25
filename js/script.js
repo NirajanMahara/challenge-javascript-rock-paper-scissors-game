e92bd255e17f53eb67a55d646f91017d742ed0e1
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-25 08:08:00 +0545

    Hide `.contest` div from the styles `display: none` & showing from `js` using `style.display: 'flex'`

--- js/script.js
+++ js/script.js
@@ -4,4 +4,8 @@ const pickUserHand = (hand) => {
   // hide the current page
   let hands = document.querySelector('.hands');
   hands.style.display = 'none';
+
+  // show the next page with the hand we picked
+  let contest = document.querySelector('.contest');
+  contest.style.display = 'flex';
 };
